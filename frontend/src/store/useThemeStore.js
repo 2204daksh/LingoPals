f1e52cce4b925e7bab9d83e4836d6ce9a4e7f823
2204daksh <dakshtrehan2204@gmail.com>
2025-07-26 16:30:00 +0530

    Update project files

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -2,9 +2,9 @@ import { create } from 'zustand'
 
 // set theme in local storage also so that the user does not have to make changes again
 export const useThemeStore = create((set) => ({
-    theme : localStorage.getItem("streamify-theme") || "dark",
+    theme : localStorage.getItem("lingopals-theme") || "dark",
     setTheme: (theme) => {
-        localStorage.setItem("streamify-theme", theme)
+        localStorage.setItem("lingopals-theme", theme)
         set({ theme })
     }
 }));
